3907ed8831cb9a1827d11bc8ccecd9f4a7b82836
sean hughes <seanx@seans-MacBook-Pro.local>
2022-02-13 21:09:42 -0800

    added things

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Opensea from "./img/opensea.svg";
 const Container = styled.div`
   max-width: 1000px;
   margin: auto;
-  padding-top: 100px;
+  padding-top: 200px;
 `;
 
 const ProfImage = styled.img`
@@ -76,15 +76,15 @@ const SidebarLink = styled.a`
     color: #E16262;
   }
   & svg {
-    font-size: 24px;
+    font-size: 29px;
   }
 `;
 
 const SideBarLinkImg = styled.img`
     background: #463A4A;
-    width: 20px;
+    width: 23px;
     border-radius: 2px;
-    margin-top: 2px;
+    margin-top: 3px;
     &:hover {
       background: #E16262;
     }
@@ -114,7 +114,7 @@ function App() {
 {/*        <ProfImage/>
         <HorSpace size="40px"/>*/}
         <div>
-          <Body>I lead product management for delivery offline batching & advanced matching @ <BodyEm>Uber</BodyEm>, in the <LinkTab href="https://www.wsj.com/articles/for-doordash-and-uber-eats-the-future-is-everything-in-about-an-hour-11622453401">frontier of the delivery space</LinkTab>. The spaces at Uber where <LinkTab href="https://www.bloomberg.com/news/articles/2021-04-22/uber-lyft-pay-big-incentives-to-get-drivers-back-on-the-road-after-covid">human behavior and marketplace economics meet</LinkTab> are my favorite to dig into.</Body>
+          <Body>I lead product management for delivery offline batching & advanced matching @ <BodyEm>Uber</BodyEm>, in the <LinkTab href="https://www.wsj.com/articles/for-doordash-and-uber-eats-the-future-is-everything-in-about-an-hour-11622453401">frontier of the delivery space</LinkTab>. My favorite spaces to dig into at Uber are where <LinkTab href="https://www.bloomberg.com/news/articles/2021-04-22/uber-lyft-pay-big-incentives-to-get-drivers-back-on-the-road-after-covid">human behavior and marketplace economics meet</LinkTab>.</Body>
           <Body>Before Uber, I graduated from <BodyEm>Harvard</BodyEm> where I studied computer science and captained the men's varsity <BodyEm>squash</BodyEm> team to <LinkTab href="https://gocrimson.com/sports/mens-squash/roster/sean-hughes/6640">2 national championships</LinkTab>. Previously, I was a 3x US national junior squash champion.</Body>
           <Body>I'm currently living with a friends in San Francisco. Send me an email at seanhughes610@gmail.com if you'd like to discuss Uber, <BodyEm>Crypto</BodyEm>, or catch up.</Body>
           <Body style={{"margin-top": "10px"}}>- Sean (2021)</Body>
